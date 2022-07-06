6f593a6f51e1082dd74f230d8092cdd63c471648
aryaman-titan <aryamanbehera@gmail.com>
2022-07-06 18:02:00 +0530

    contact form integrated with API
    Signed-off-by: aryaman-titan <aryamanbehera@gmail.com>

--- src/common/utils/useForm.tsx
+++ src/common/utils/useForm.tsx
@@ -33,7 +33,7 @@ export const useForm = (validate: any) => {
         event.preventDefault();
         setErrors(validate(values));
 
-        const webhookURL = "https://hooks.slack.com/services/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";
+        const webhookURL = "https://hooks.slack.com/services/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";
 
         const formattedData = formatContactData(values);
         console.log(formattedData);
